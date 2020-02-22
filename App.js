c19b37d29d4c71951d4433f4c4fd987af67b20a9
Carvalho, Vinicius Luiz <carvalho.viniciusluiz@gmail.com>
2020-02-22 16:28:00 -0300

    feat: Adding posts

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Header from './src/components/Header';
 import Stories from './src/components/Stories';
+import Posts from './src/components/Posts';
 
 const App = () => {
   return (
     <React.Fragment>
       <Header/>
       <Stories/>
+      <Posts/>
     </React.Fragment>
   )
 }
